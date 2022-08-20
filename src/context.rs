2747183ec0282f9d356144b8f7f87b3806815575
Caesar X Insanium <jesusramos.official@gmail.com>
2022-08-20 15:53:13 -0500

    simple abstractions

---
+++ src/context.rs
@@ -0,0 +1,182 @@
+use crate::vertex::{Vertex, Mesh};
+use wgpu::{self, util::DeviceExt};
+
+pub struct Context {
+    clear_color: wgpu::Color,
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+    meshes: Vec<Mesh>,
+}
+
+impl Context {
+    pub async fn new(window: &winit::window::Window) -> Self {
+        let clear_color = wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        };
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: true,
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    // extra features that are not guranteed to be available
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: Some("Device"),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_supported_formats(&adapter)[0],
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        surface.configure(&device, &config);
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::desc()],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+        let meshes = Vec::new();
+        Self {
+            clear_color,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline,
+            meshes,
+        }
+    }
+    pub fn add_mesh(&mut self, vertices: &[Vertex], indices: &[u16]){
+
+        let vertex_buffer = self.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = self.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        self.meshes.push(Mesh{
+            vertex_buffer,
+            index_buffer,
+            count: indices.len() as u32,
+        })
+    }
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+    #[allow(unused)]
+    pub fn set_clear_color(&mut self, new: wgpu::Color) {
+        self.clear_color = new;
+    }
+    #[allow(unused)]
+    pub fn update_uniforms(&mut self) {}
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+        {
+            // Describe pipeline that will be used in order to render something to the screen
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                // describe operations that will be peformed in before render pass
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+            render_pass.set_pipeline(&self.render_pipeline);
+            for mesh in self.meshes.iter() {
+                render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
+                render_pass
+                    .set_index_buffer(mesh.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                render_pass.draw_indexed(0..mesh.count, 0, 0..1);
+            }
+        }
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 mod app;
+mod context;
 mod vertex;
-use app::run;
-// this is the main fuction
+use context::Context;
+use app::AppState;
+use winit::{event_loop::EventLoop, window::WindowBuilder, dpi::LogicalSize};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
+
+/// this is the main fuction
 fn main() {
-    run();
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("WGPU Learning")
+        .with_resizable(false)
+        .with_inner_size(LogicalSize {
+            width: WIDTH,
+            height: HEIGHT,
+        })
+        .build(&event_loop)
+        .unwrap();
+    
+    // WGPU
+    let mut context = pollster::block_on(Context::new(&window));
+    context.add_mesh(vertex::VERTICES, vertex::INDICES);
+
+    let mut app = AppState::new(context);
+    event_loop.run(move |event, _, control_flow| {
+        app.handle_event_cycle(&window, event, control_flow);
+    });
 }
